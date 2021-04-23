d268a177d63d813bd303f9e927a739814f7d1a4d
Nes B <anes.bkdm@live.fr>
2021-04-23 09:52:47 +0200

    Added DEX Wallet and test coin

---
+++ contracts/Wallet.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+/**
+ * @author Nes B.
+ * @dev Wallet implementation for TofuDEX.
+ */
+
+pragma solidity ^0.8.0;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol/";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol/";
+
+contract Wallet is Ownable{
+
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    bytes32[] public tokenList;
+
+    mapping(bytes32 => Token) public tokenMapping;
+    mapping(address => mapping(bytes32 => uint256)) public balances;
+
+    modifier isToken(bytes32 _ticker) {
+        require(tokenMapping[_ticker].tokenAddress != address(0), "Token doesn't exist in Tofu.");
+        _;
+    }
+
+    function addToken(bytes32 _ticker, address _tokenAddress) external onlyOwner {
+        tokenMapping[_ticker] = Token(_ticker, _tokenAddress);
+        tokenList.push(_ticker);
+    }
+
+    function deposit(uint _amount, bytes32 _ticker) external isToken(_ticker) {
+        IERC20(tokenMapping[_ticker].tokenAddress).transferFrom(msg.sender, address(this), _amount);
+        balances[msg.sender][_ticker] += _amount;
+    }
+
+    function withdraw(uint _amount, bytes32 _ticker) external isToken(_ticker) {
+        require(balances[msg.sender][_ticker] >= _amount, "Not enough money.");
+        balances[msg.sender][_ticker] -= _amount;
+        IERC20(tokenMapping[_ticker].tokenAddress).transfer(msg.sender,_amount);
+    }
+}
+
